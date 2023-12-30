874eb8c0004ec41824185b708fff8aeae2d8ee53
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2023-12-31 00:44:34 +0530

    autocomplete off

--- app/(dashboard)/(routes)/dsa-cp/(routes)/problems/(routes)/new/page.tsx
+++ app/(dashboard)/(routes)/dsa-cp/(routes)/problems/(routes)/new/page.tsx
@@ -204,6 +204,7 @@ const AddProblem = () => {
                                 type="text"
                                 name="password"
                                 id="password"
+                                autoComplete="off"
                                 className="block w-full rounded-md border-0 p-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 placeholder="coding75 Password"
                                 required />
