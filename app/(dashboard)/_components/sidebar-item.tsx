e2fd0bcf7174f5a868dad265c5db73181b1e0852
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2024-01-27 13:40:57 +0530

    check phone

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -25,10 +25,11 @@ export const SidebarItem = ({
     pathname?.startsWith(`${href}/`);
 
   return (
+    <>
     <Link
       href={href}
       className={cn(
-        "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
+        "hidden lg:flex md:flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
         isActive && "text-indigo-700 bg-indigo-200/20 hover:bg-indigo-200/20 hover:text-indigo-700"
       )}//text-sky-700 previous color
     >
@@ -40,7 +41,7 @@ export const SidebarItem = ({
             isActive && "text-indigo-700"
           )}
         />
-        {label} 
+        {label}
         {/* <span className="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">Badge</span> */}
       </div>
       <div
@@ -50,5 +51,30 @@ export const SidebarItem = ({
         )}
       />
     </Link>
+    <a
+      href={href}
+      className={cn(
+        "lg:hidden md:hidden flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
+        isActive && "text-indigo-700 bg-indigo-200/20 hover:bg-indigo-200/20 hover:text-indigo-700"
+      )}//text-sky-700 previous color
+    >
+      <div className="flex items-center gap-x-2 py-4">
+        <Icon
+          size={22}
+          className={cn(
+            "text-slate-500",
+            isActive && "text-indigo-700"
+          )}
+        />
+        {label}
+        {/* <span className="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">Badge</span> */}
+      </div>
+      <div
+        className={cn(
+          "ml-auto opacity-0 border-2 border-indigo-700 h-full transition-all",
+          isActive && "opacity-100"
+        )}
+      />
+    </a></>
   )
 }
