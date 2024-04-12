bdfda7dddca6211336c02b4fe1d623de1409f2f6
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2024-04-13 03:33:10 +0530

    notification alert update

--- app/(dashboard)/_components/sidebar/navbar-routes.tsx
+++ app/(dashboard)/_components/sidebar/navbar-routes.tsx
@@ -37,6 +37,7 @@ import { LinkedInLogoIcon } from "@radix-ui/react-icons";
 import { feedback_form, github_link, linkedin_link, telegram_link, youtube_link } from "@/components/social-links";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Logo } from "../components/logo";
+import { Separator } from "@/components/ui/separator";
 
 const components: { title: string; href: string; description: string }[] = [
   {
@@ -326,7 +327,7 @@ export const NavbarRoutes = ({ isLogo = false }: any) => {
             <DropdownMenu>
               <div className="relative">
                 <a href={github_link} target="_blank">
-                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg>
                 </a>
               </div>
             </DropdownMenu>
@@ -373,7 +374,6 @@ export const NavbarRoutes = ({ isLogo = false }: any) => {
                   <div
                     className="absolute bottom-auto left-auto right-0 top-0 z-10 inline-block rotate-0 skew-x-0 skew-y-0 scale-x-100 scale-y-100 rounded-full bg-red-600 p-1.5 text-xs"></div>
                 }
-
                 <div
                   className="cursor-pointer hover:shadow-none flex items-center justify-center rounded-full bg-primary-700 p-2 text-center text-white shadow-xl dark:text-gray-200">
                   <Bell className="md:h-5 md:w-5 h-3 w-3" />
@@ -381,7 +381,11 @@ export const NavbarRoutes = ({ isLogo = false }: any) => {
               </div>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
-              <DropdownMenuItem className="cursor-pointer">Welcome to coding75!</DropdownMenuItem>
+              <Link href="/pro">
+                <DropdownMenuItem className="cursor-pointer px-5 py-3">coding75 Pro - Launching Soon 🚀</DropdownMenuItem>
+              </Link>
+              <Separator/>
+              <DropdownMenuItem className="cursor-pointer px-5 py-3">Welcome to coding75!</DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
