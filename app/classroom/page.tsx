815ede2d92d876bcf5c774b8ed80f02aa336ae3d
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2024-04-27 11:16:43 +0530

    build fix for classroom

--- app/classroom/page.tsx
+++ app/classroom/page.tsx
@@ -2,10 +2,13 @@
 
 import { useRouter } from "next/navigation";
 import { Logo } from "../(dashboard)/_components/components/logo";
+import { useEffect } from "react";
 
 const Classroom = () => {
     const router = useRouter()
-    router.push("/classroom/dashboard")
+    useEffect(()=>{
+        router.replace("/classroom/dashboard")
+    },[])
     return (
         <div className="min-h-screen animate-ping flex justify-center items-center "><Logo/></div>
     );
