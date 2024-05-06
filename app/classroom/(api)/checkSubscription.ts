8a096e39df26a45a48fed69872a0658813b25ea1
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2024-05-06 13:39:23 +0530

    bug fix for local key set

--- app/classroom/(api)/checkSubscription.ts
+++ app/classroom/(api)/checkSubscription.ts
@@ -4,11 +4,11 @@ import { toast } from "sonner";
 export async function checkSubscription(user_email) {
     const url = process?.env?.NEXT_PUBLIC_REGISTRATION_SHEET;
     let token = (new Date().toISOString().slice(0, 10)) + user_email
+    if (localStorage?.getItem("pro-token")&&decrypt(localStorage?.getItem("pro-token")) == token) {
+        console.log("done");
+        return true
+    }
     try {
-        if (decrypt(localStorage?.getItem("pro-token")) == token) {
-            console.log("done");
-            return true
-        }
         const res = await fetch(url || "")
         const data = await res.json()
         for (let i = 1; i < data?.["content"]?.length; i++) {
