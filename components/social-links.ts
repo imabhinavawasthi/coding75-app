f42c7ab5fd13a9f89ed728ff9ce867c9fe15e176
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2024-05-06 15:10:40 +0530

    added feedback form

--- components/social-links.ts
+++ components/social-links.ts
@@ -8,7 +8,7 @@ export const youtube_link = "https://youtube.com/@crackdsa"
 
 export const feedback_form = "https://forms.gle/S5KKUyrUqi1WzZCJ6"
 
-export const pro_feedback_form = "https://forms.gle/S5KKUyrUqi1WzZCJ6"
+export const pro_feedback_form = "https://forms.gle/NEfBozxQzhRkv1kq9"
 
 export const whatsapp_link = "https://wa.me/message/TPN76XLWVOWDB1"
 
